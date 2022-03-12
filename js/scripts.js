04f4b71444f252048337a94554f15c3bb8e54036
catherineotondi <catherineotondi@gmail.com>
2022-03-12 23:26:28 +0300

    added new paragraph in the what we do sector titled design in the index file
    added a new paragraph in the what we do section titled Development in the index file
    
    added new paragraphs in the what we do saction titled Product Management in the index file
    
    added jQuery toggle function in the scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".designicon").click(function(){
+        $(".initially-showing").toggle();//initally can be used incase you have several elements in your
+        // $(".initially-hidden").toggle();//documents that require the same styling.
+    });
+});
