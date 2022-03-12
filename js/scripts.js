1b80d0176eae73395360a69925c6d1654f1de8a3
catherineotondi <catherineotondi@gmail.com>
2022-03-13 02:50:46 +0300

    created a submit button in the index file
    added the required attribute in the form elements of the index file
    
    added the form tags in the inex file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     $(".designicon").click(function(){
         $(".initially-showing").toggle();//initally can be used incase you have several elements in your
-        // $(".initially-hidden").toggle();//documents that require the same styling.
+        $(".designicon").toggle();//documents that require the same styling.
     });
+});
+
+$("#submit").click(function(){
+alert("We have recived your meassage.Thank you for contactin us")
 });
