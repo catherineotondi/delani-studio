11bc25dd82d2d567935134e2cccb186ad9a4e186
catherineotondi <catherineotondi@gmail.com>
2022-03-14 13:22:44 +0300

    added a mailchimp in the index file
    deleted the display none in the stylesheet.css file
    
    added a hide function in the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
+    $('.design-des, .dev-des, #product').hide()
     // toggle between the icons and description elements.
     $(".designiconn").on('click', function (){
-        $(".design-des").fadeToggle(3000);
-        $(".designicon").fadeToggle(3000);
+        
+        $(".design-des").fadeToggle(300);
+        $(".designicon").fadeToggle(300);
     });
     $(".deviconn").on('click', function (){
-        $(".dev-des").fadeToggle(3000);
-        $(".devicon").fadeToggle(3000);
+        $(".dev-des").fadeToggle(300);
+        $(".devicon").fadeToggle(300);
     });
     $(".producticonn").on('click', function (){
-        $("#product").fadeToggle(3000);
-        $("producticon").fadeToggle(3000);
+        $("#product").fadeToggle(300);
+        $("producticon").fadeToggle(300);
     });
+    $('#form').on('submit', function (e) {
+            e.preventDefault()
 
     $("#submit").click(function(){
         alert("We have received your message. Thank you for contacting us.");
@@ -19,6 +23,4 @@ $(document).ready(function(){
     
 });
 
-
-
-   
+ });
